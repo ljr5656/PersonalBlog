745977b8a45c6e62d3bcf640cf6db2f5a8e581b2
ljr5656 <38825418+ljr5656@users.noreply.github.com>
2019-09-17 21:51:53 +0800

    Update DBUtil.js

--- dao/DBUtil.js
+++ dao/DBUtil.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 //连接数据库
 function createConnection() {
     var connection = mysql.createConnection({
-        host: "127.0.0.1",
+        host: "192.168.56.1",
         port: "3306",
         user: "root",
         password: "l19980305",
